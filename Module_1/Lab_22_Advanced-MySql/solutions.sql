c1476f684a258f682d6d53aa24050b6b984e1265
marciafof <mfotaloraf@gmail.com>
2023-01-27 00:51:50 +0100

    COMPLETED: Corrected Lab2

--- Module_1/Lab_22_Advanced-MySql/solutions.sql
+++ Module_1/Lab_22_Advanced-MySql/solutions.sql
@@ -9,7 +9,6 @@ SELECT * FROM sales LIMIT 1;
 
 
 /* Write a *SELECT* query to obtain the following output:
-
 * Title ID
 * Author ID
 * Advance of each title and author
@@ -24,44 +23,29 @@ SELECT * FROM sales LIMIT 1;
         ```
     * Note that `titles.royalty` and `titleauthor.royaltyper` are divided by 100 respectively because they are percentage numbers instead of floats.
 
-In the output of this step, each title may appear more than once for each author. This is because a title can have more than one sale. 
 */
 SELECT * FROM titleauthor ORDER BY title_id DESC;
 
 -- ROYALTY PERCENTAGE varies because one title can have several authors so pervcentage <100
+--  SELECT * FROM titleauthor WHERE royaltyper IS NOT NULL;
+-- create view view1 as
+-- Creating ONE single "table" to get an easier view
 SELECT titleauthor.title_id, titleauthor.au_id, 
-	(titles.advance*titleauthor.royaltyper/100) as advance
-FROM titleauthor 
+		CONCAT(authors.au_fname, " ", authors.au_lname) as au_completename,
+		titles.price,
+		titles.royalty, titleauthor.royaltyper, titles.advance,
+        sales.qty, 
+        (titles.advance*titleauthor.royaltyper/100) as advance_per_au,
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+FROM titleauthor
+INNER JOIN authors
+ON titleauthor.au_id = authors.au_id
 INNER JOIN titles
 ON titleauthor.title_id = titles.title_id
-HAVING advance IS NOT NULL;
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id
+ORDER BY title_id, au_id;
 
---  SELECT * FROM titleauthor WHERE royaltyper IS NOT NULL;
-/* ONE LINER 
-
-SELECT COUNT(*)
-FROM(
-SELECT s.title_id,
-       a.au_id,
-       (t.advance * ta.royaltyper)/100 as advance_title_author,
-       (t.price * s.qty * t.royalty)/100*ta.royaltyper/100 as sales_royalty
-FROM sales as s, authors as a, titles as t, titleauthor as ta) as oneliner;
-*/
-
--- SELECT COUNT(*)
--- FROM(
-SELECT advancetable.title_id, advancetable.au_id, advancetable.advance,
-		(advancetable.price * sales.qty * advancetable.royalty / 100 * advancetable.royaltyper / 100) as sales_royalty
-FROM (
-		SELECT titleauthor.title_id, titleauthor.au_id, titles.price,
-		titles.royalty, titleauthor.royaltyper,
-			(titles.advance*titleauthor.royaltyper/100) as advance
-		FROM titleauthor 
-		LEFT JOIN titles
-		ON titleauthor.title_id = titles.title_id
-        WHERE advance IS NOT NULL) as advancetable
-LEFT JOIN sales
-ON advancetable.title_id = sales.title_id; -- as complicated;
 
 ### Step 2: Aggregate the total royalties for each title and author
 
@@ -73,83 +57,99 @@ ON advancetable.title_id = sales.title_id; -- as complicated;
     * Hint: use the *SUM* subquery and group by both `au_id` and `title_id`
 
 In the output of this step, each title should appear only once for each author. */
-SELECT tableroyalty.au_id, tableroyalty.title_id, SUM(tableroyalty.sales_royalty) as agg_royalties
+-- AGGREGATING the calculated royalties by title by author (ONLY SUM)
+SELECT tableroyalty.au_id, tableroyalty.title_id, tableroyalty.au_completename, tableroyalty.advance_per_au, SUM(tableroyalty.sales_royalty) as agg_royalties
 	FROM (
-		SELECT advancetable.title_id, advancetable.au_id, advancetable.advance,
-			(advancetable.price * sales.qty * advancetable.royalty / 100 * advancetable.royaltyper / 100) as sales_royalty
-		FROM (
-				SELECT titleauthor.title_id, titleauthor.au_id, titles.price,
-				titles.royalty, titleauthor.royaltyper,
-					(titles.advance*titleauthor.royaltyper/100) as advance
-				FROM titleauthor 
-				LEFT JOIN titles
-				ON titleauthor.title_id = titles.title_id
-                WHERE advance IS NOT NULL) as advancetable
-		LEFT JOIN sales
-		ON advancetable.title_id = sales.title_id) AS tableroyalty
+		SELECT titleauthor.title_id, titleauthor.au_id, 
+		CONCAT(authors.au_fname, " ", authors.au_lname) as au_completename,
+		titles.price,
+		titles.royalty, titleauthor.royaltyper, titles.advance,
+        sales.qty, 
+        (titles.advance*titleauthor.royaltyper/100) as advance_per_au,
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+		FROM titleauthor
+		INNER JOIN authors
+		ON titleauthor.au_id = authors.au_id
+		INNER JOIN titles
+		ON titleauthor.title_id = titles.title_id
+		INNER JOIN sales
+		ON titleauthor.title_id = sales.title_id
+		ORDER BY title_id, au_id
+		) AS tableroyalty
+	GROUP BY  tableroyalty.title_id,tableroyalty.au_id;
+/*
+#WITH VIEW
+select * from view1;
+
+create view viewaggroyalties as
+SELECT tableroyalty.au_id, tableroyalty.title_id, tableroyalty.au_completename, tableroyalty.advance_per_au, SUM(tableroyalty.sales_royalty) as agg_royalties
+	FROM view1 AS tableroyalty
 	GROUP BY  tableroyalty.au_id, tableroyalty.title_id;
-
-
-SELECT SUM(agg_royalties)
-FROM
-	(
-	SELECT tableroyalty.au_id, tableroyalty.title_id, SUM(tableroyalty.sales_royalty) as agg_royalties
-	FROM (
-		SELECT advancetable.title_id, advancetable.au_id, advancetable.advance,
-			(advancetable.price * sales.qty * advancetable.royalty / 100 * advancetable.royaltyper / 100) as sales_royalty
-		FROM (
-				SELECT titleauthor.title_id, titleauthor.au_id, titles.price,
-				titles.royalty, titleauthor.royaltyper,
-					(titles.advance*titleauthor.royaltyper/100) as advance
-				FROM titleauthor 
-				LEFT JOIN titles
-				ON titleauthor.title_id = titles.title_id
-                WHERE advance IS NOT NULL) as advancetable
-		LEFT JOIN sales
-		ON advancetable.title_id = sales.title_id) AS tableroyalty
-	GROUP BY  tableroyalty.au_id, tableroyalty.title_id) as result;
     
-    
+select * from viewaggroyalties;
+SELECT totalprofittable.au_id, (SUM(totalprofittable.advance_per_au)+SUM(totalprofittable.agg_royalties)) as totalprofit,
+		totalprofittable.au_completename
+FROM viewaggroyalties AS totalprofittable
+GROUP BY  totalprofittable.au_id
+ORDER BY totalprofit DESC LIMIT 3
+    ;
+*/
+
 ### Step 3: Calculate the total profits of each author
-
-SELECT tableroyalty.au_id, SUM(tableroyalty.sales_royalty) as agg_royalties
+/* Using the output from Step 2, write a query, containing two subqueries, to obtain the following output:
+Author ID
+Profits of each author by aggregating the advance and total royalties of each title
+Sort the output based on a total profits from high to low, and limit the number of rows to 3.*/
+SELECT totalprofittable.au_id, (SUM(totalprofittable.advance_per_au)+SUM(totalprofittable.agg_royalties)) as totalprofit,
+	   totalprofittable.au_completename
+FROM(
+		SELECT tableroyalty.au_id, tableroyalty.title_id, tableroyalty.au_completename, tableroyalty.advance_per_au, SUM(tableroyalty.sales_royalty) as agg_royalties
 	FROM (
-		SELECT advancetable.title_id, advancetable.au_id, advancetable.advance,
-			(advancetable.price * sales.qty * advancetable.royalty / 100 * advancetable.royaltyper / 100) as sales_royalty
-		FROM (
-				SELECT titleauthor.title_id, titleauthor.au_id, titles.price,
-				titles.royalty, titleauthor.royaltyper,
-					(titles.advance*titleauthor.royaltyper/100) as advance
-				FROM titleauthor 
-				LEFT JOIN titles
-				ON titleauthor.title_id = titles.title_id
-                WHERE advance IS NOT NULL) as advancetable
-		LEFT JOIN sales
-		ON advancetable.title_id = sales.title_id) AS tableroyalty
-	GROUP BY  tableroyalty.au_id
-    ORDER BY agg_royalties DESC LIMIT 3
+		SELECT titleauthor.title_id, titleauthor.au_id, 
+		CONCAT(authors.au_fname, " ", authors.au_lname) as au_completename,
+		titles.price,
+		titles.royalty, titleauthor.royaltyper, titles.advance,
+        sales.qty, 
+        (titles.advance*titleauthor.royaltyper/100) as advance_per_au,
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+		FROM titleauthor
+		INNER JOIN authors
+		ON titleauthor.au_id = authors.au_id
+		INNER JOIN titles
+		ON titleauthor.title_id = titles.title_id
+		INNER JOIN sales
+		ON titleauthor.title_id = sales.title_id
+		ORDER BY title_id, au_id
+		) AS tableroyalty
+	GROUP BY  tableroyalty.au_id, tableroyalty.title_id
+    )
+    AS totalprofittable
+GROUP BY  totalprofittable.au_id
+ORDER BY totalprofit DESC LIMIT 3
     ;
 
 /* ALTERNATIVE SOLUTION */
 #CSTEP 1
 
-CREATE TEMPORARY TABLE publications.royaltypsale
-
-SELECT advancetable.title_id, advancetable.au_id, advancetable.advance,
-		(advancetable.price * sales.qty * advancetable.royalty / 100 * advancetable.royaltyper / 100) as sales_royalty
-FROM (
-		SELECT titleauthor.title_id, titleauthor.au_id, titles.price,
-		titles.royalty, titleauthor.royaltyper,
-			(titles.advance*titleauthor.royaltyper/100) as advance
-		FROM titleauthor 
-		LEFT JOIN titles
-		ON titleauthor.title_id = titles.title_id
-        WHERE advance IS NOT NULL) as advancetable
-LEFT JOIN sales
-ON advancetable.title_id = sales.title_id; -- as complicated;
+CREATE TEMPORARY TABLE publications.royaltypsaleau
+SELECT titleauthor.title_id, titleauthor.au_id, 
+		CONCAT(authors.au_fname, " ", authors.au_lname) as au_completename,
+		titles.price,
+		titles.royalty, titleauthor.royaltyper, titles.advance,
+        sales.qty, 
+        (titles.advance*titleauthor.royaltyper/100) as advance_per_au,
+        (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+FROM titleauthor
+INNER JOIN authors
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id
+ORDER BY title_id, au_id;
 
 SELECT *
-FROM publications.royaltypsale
+FROM publications.royaltypsaleau
 LIMIT 1;
 
 ### Step 2: Aggregate the total royalties for each title and author
@@ -162,21 +162,22 @@ LIMIT 1;
     * Hint: use the *SUM* subquery and group by both `au_id` and `title_id`
 
 In the output of this step, each title should appear only once for each author. */
-SELECT tableroyalty.au_id, tableroyalty.title_id, SUM(tableroyalty.sales_royalty) as agg_royalties
-FROM publications.royaltypsale as tableroyalty
-GROUP BY  tableroyalty.au_id, tableroyalty.title_id;
+CREATE TEMPORARY TABLE publications.royaltyptitleau
+SELECT tableroyalty.au_id, tableroyalty.title_id, tableroyalty.au_completename, tableroyalty.advance_per_au, SUM(tableroyalty.sales_royalty) as agg_royalties
+FROM publications.royaltypsaleau as tableroyalty
+GROUP BY tableroyalty.title_id, tableroyalty.au_id,tableroyalty.au_completename, tableroyalty.advance_per_au;
 
-
-SELECT SUM(agg_royalties)
-FROM
-	(SELECT tableroyalty.au_id, tableroyalty.title_id, SUM(tableroyalty.sales_royalty) as agg_royalties
-FROM publications.royaltypsale as tableroyalty
-GROUP BY  tableroyalty.au_id, tableroyalty.title_id) as result;
 
 ### Step 3: Calculate the total profits of each author ROYALTIES AND ADNVANCE
+SELECT totalprofittable.au_id, (SUM(totalprofittable.advance_per_au)+SUM(totalprofittable.agg_royalties)) as totalprofit,
+	   totalprofittable.au_completename
+FROM  publications.royaltyptitleau AS totalprofittable
+GROUP BY  totalprofittable.au_id,totalprofittable.au_completename
+ORDER BY totalprofit DESC LIMIT 3;
 
-SELECT tableroyalty.au_id, (SUM(tableroyalty.advance)+SUM(tableroyalty.sales_royalty)) as totalprofit
-FROM publications.royaltypsale as tableroyalty
-GROUP BY  tableroyalty.au_id
-ORDER BY totalprofit DESC LIMIT 3
-    ;
+/* CHALLENGE 3 */
+CREATE TABLE most_profiting_authors
+SELECT totalprofittable.au_id, (SUM(totalprofittable.advance_per_au)+SUM(totalprofittable.agg_royalties)) as totalprofit
+FROM  publications.royaltyptitleau AS totalprofittable
+GROUP BY  totalprofittable.au_id,totalprofittable.au_completename
+ORDER BY totalprofit DESC;
